857fdea43ed656e0b85dc85f96b1fbdd8aa8d5d3
Unknown <steven.chou@uniqestate.com>
2018-08-06 22:46:24 +0800

    annotations - setter injection

--- spring_&_hibernate/spring-demo-annotations/src/com/tronbrain/springdemo/TennisCoach.java
+++ spring_&_hibernate/spring-demo-annotations/src/com/tronbrain/springdemo/TennisCoach.java
@@ -8,10 +8,34 @@ public class TennisCoach implements Coach {
 
 	private FortuneService fortuneService;
 
-	@Autowired
-	public TennisCoach(FortuneService theFortuneService) {
-		this.fortuneService = theFortuneService;
+	// define a default constructor
+	public TennisCoach() {
+		System.out.println(">> TennisCoach: inside default constructor");
 	}
+
+	
+    /*
+    @Autowired 
+    public TennisCoach(FortuneService theFortuneService) {
+    	this.fortuneService = theFortuneService;
+    }
+    */
+	 
+
+	// define a setter method
+	@Autowired
+	public void setFortuneService(FortuneService fortuneService) {
+		System.out.println(">> TennisCoach: inside setFortuneService");
+		this.fortuneService = fortuneService;
+	}
+	
+	/*
+	@Autowired
+	public void doSomeCrazyStuff(FortuneService fortuneService) {
+		System.out.println(">> TennisCoach: inside doSomeCrazyStuff");
+		this.fortuneService = fortuneService;
+	}
+	*/
 
 	@Override
 	public String getDailyWorkout() {
